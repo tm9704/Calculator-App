277aeac76fec4ae9aca42e876dea2a926ba294f4
tm9704 <tm9704@korea.ac.kr>
2022-08-25 17:03:52 +0900

    CalcApp: UI_1

--- app/src/main/java/com/tm9704/mycalculator/MainActivity.kt
+++ app/src/main/java/com/tm9704/mycalculator/MainActivity.kt
@@ -2,10 +2,16 @@ package com.tm9704.mycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View){
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
     }
 }
