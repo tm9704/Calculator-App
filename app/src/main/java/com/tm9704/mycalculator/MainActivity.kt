1dcf044644739b80ced69406a164b6494b79b61d
tm9704 <tm9704@korea.ac.kr>
2022-09-01 12:39:24 +0900

    CalcApp: Adding onDecimalPoint Function

--- app/src/main/java/com/tm9704/mycalculator/MainActivity.kt
+++ app/src/main/java/com/tm9704/mycalculator/MainActivity.kt
@@ -10,6 +10,8 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     private var tvInput: TextView? = null
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +22,19 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View){
         tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
     }
 
     fun onClear(view: View){
         tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view:View){
+        if(lastNumeric && !lastDot){
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 }
